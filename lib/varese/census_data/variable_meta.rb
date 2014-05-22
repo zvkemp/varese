91c607402315a8076abde1d900464d9e4269055d
zvkemp <zvkemp@gmail.com>
2014-05-22 12:57:52 -0700

    variable meta types

--- lib/varese/census_data/variable_meta.rb
+++ lib/varese/census_data/variable_meta.rb
@@ -1,13 +1,20 @@
 module Varese
   module CensusData
     class VariableMeta < Meta
-      attr_reader :guid, :label, :concept, :concept_id
+      attr_reader :guid, :label, :concept, :concept_id, :type
 
       def initialize(guid, data)
         @guid       = guid
         @label      = data["label"]
         @concept    = data["concept"]
         @concept_id = "#{data["concept"]}".split(".").first
+        @type       = derive_type_from_guid(guid)
+      end
+
+      private
+
+      def derive_type_from_guid(guid)
+        { "E" => :estimate, "M" => :margin_of_error }.fetch("#{guid}".chars.last, :unknown)
       end
     end
   end
--- spec/varese/census_data/variable_meta_spec.rb
+++ spec/varese/census_data/variable_meta_spec.rb
@@ -1,9 +1,13 @@
 require_relative '../../spec_helper'
 
 describe Varese::CensusData::VariableMeta do
+  let(:klass){ Varese::CensusData::VariableMeta }
   let(:guid){ "B02001_006E" }
   let(:raw){ { "label" => "Native Hawaiian and Other Pacific Islander alone", "concept" => "B02001.  Race" } }
-  let(:variable_meta){ Varese::CensusData::VariableMeta.new(guid, raw) }
+  let(:variable_meta){ klass.new(guid, raw) }
 
   specify { variable_meta.concept_id.must_equal "B02001" }
+  specify { variable_meta.type.must_equal :estimate }
+  specify { klass.new("B02001_006M", {}).type.must_equal :margin_of_error }
+  specify { klass.new("B02001_006U", {}).type.must_equal :unknown }
 end
